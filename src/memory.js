3b5bece2aa8d3a7e44b37f47700958d888946c28
Alexander Alexy <ax81@airvonalexander.localdomain>
2023-06-22 08:09:06 +0200

    Solved lab

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,70 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    // returns undefined if no argument is passed to the function 
+
+    if(!this.cards) {
+      return undefined;
+    }
+
+    // Loop to iterate the cards array
+    for(let i = 0; i < this.cards.length; i += 1) {
+  
+    // Saves current item to a temp var
+      let temp = this.cards[i];
+    
+    // Generates a random number in the range of the array
+      let r = Math.floor(Math.random() * this.cards.length);
+
+    // Get the item at the random index and ..
+    // replace the current item with the random item
+      this.cards[i] = this.cards[r];
+
+    // Replace the current item with the current item as temp
+      this.cards[r] = temp;
+    }
+
+    return this.cards;
   }
 
+
   checkIfPair(card1, card2) {
-    // ... write your code here
+    
+  // Update pairsClicked with +1 and returns false if there is no pair 
+    if(card1 !== card2 ) {
+      this.pairsClicked++;
+      return false;
+    }
+
+  // Update pairsClicked and pairsGuessed by +1 
+  // returns returns true, because of pair 
+    else {
+      this.pairsClicked++;
+      this.pairsGuessed++;
+      return true;
+    }
+
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+
+    
+
+    if( this.pairsGuessed < 8) {
+      return false;
+    }
+   
+    else {
+     
+      return true;
+    }
   }
 }
